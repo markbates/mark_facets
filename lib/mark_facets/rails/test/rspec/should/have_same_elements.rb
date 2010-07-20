fcb869250c7cd3c51fa717bbb9b95f91776ae8f8
Mark Bates <mark@markbates.com>
2010-07-20 15:37:59 -0400

    Added a rails 3 version of have_same_elements

--- lib/mark_facets/rails/test/rspec/should/have_same_elements.rb
+++ lib/mark_facets/rails/test/rspec/should/have_same_elements.rb
@@ -1,8 +1,28 @@
 if Rails.v3?
   
+  def have_same_elements(expected)
+    Rspec::Matchers::Matcher.new :have_same_elements, expected do
+      match do |actual|
+        # matcher.failure_message = "expected #{given.inspect} to include the same elements as #{expected.inspect}"
+        # matcher.negative_failure_message = "expected #{given.inspect} not to include the same elements as #{expected.inspect}"
+        unless actual.size == expected.size
+          false
+        else
+          ret = true
+          actual.each do |v|
+            ret = expected.include?(v)
+            break if !ret
+          end
+          ret
+        end
+      end
+    end
+  end
+  
 end
 
 if Rails.v2?
+  
   def have_same_elements(expected)
     simple_matcher("equal #{expected.inspect}") do |given, matcher|
       matcher.failure_message = "expected #{given.inspect} to include the same elements as #{expected.inspect}"
@@ -19,4 +39,5 @@ if Rails.v2?
       end
     end
   end
+  
 end
