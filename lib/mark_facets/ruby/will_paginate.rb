e8d121ab46d44cd6c9a156a696377ffa25277843
Mark Bates <mark@markbates.com>
2010-12-24 14:39:41 -0500

    fixed a bug with per_page in to_will_paginate

--- lib/mark_facets/ruby/will_paginate.rb
+++ lib/mark_facets/ruby/will_paginate.rb
@@ -3,7 +3,7 @@ if defined?(WillPaginate)
   class Array
     
     def to_will_paginate(options = {})
-      options.reverse_merge!({:page => 1, :per_page => self.size, :total => self.size})
+      options.reverse_merge!({:page => 1, :per_page => self.size + 1, :total => self.size})
       results = WillPaginate::Collection.create(options[:page], options[:per_page], options[:total]) do |pager|
         pager.replace(self)
       end
